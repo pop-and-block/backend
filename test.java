2795fdddd363578d901db731ebcf763a0a7bbd57
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2021-05-23 19:07:20 +0530

    Added top 10 markets - res.json contains market info - added method to access res.json

--- test.java
+++ test.java
@@ -1,16 +1,40 @@
 /*
 	Test class for the API methods
-
-	TODO: List all the INR markets
 */
 
 import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.InputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class test
 {
 	public static void main(String []args)
 	{
-		// JSONObject APIdata = new JSONObject(APIw.getJSONEndPoint("trades", "btcusdt"));
-		System.out.println(APIw.getJSONEndPoint("trades", "ethinr"));
+		try(InputStream stream = Files.newInputStream(Paths.get("res.json")))
+		{
+			// Convert stream to string
+			String contents = new String(stream.readAllBytes(), StandardCharsets.UTF_8);
+
+			// To print the string content
+			// System.out.println(contents);
+
+			JSONObject resJSON = new JSONObject(contents);
+			// System.out.println(resJSON);
+
+			JSONArray markets = resJSON.getJSONArray("markets");
+
+			for(int i = 0; i < markets.length(); i++)
+				System.out.println(markets.get(i));
+		}
+		
+		catch(IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
 	}
 }
