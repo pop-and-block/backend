3c14dc6436716d76a24b77c560eeae62c2dcbb86
sana-170102 <83946650+sana-170102@users.noreply.github.com>
2021-05-23 22:09:21 +0530

    implemented for all markets

--- APIc/APIc.java
+++ APIc/APIc.java
@@ -7,29 +7,59 @@
 import org.json.JSONObject;
 import org.json.JSONArray;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.InputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class APIc
 {
 	public static void main(String []args)
 	{
-		JSONArray Coin1_Value = new JSONArray(APIw.getJSONEndPoint("trades","ethinr"));
-
-
 		JSONObject chart_stuff = new JSONObject();
-		JSONObject Coin1_FinalValue = new JSONObject();
+		try(InputStream stream = Files.newInputStream(Paths.get("res.json")))
+		{
+			// Convert stream to string
+			String contents = new String(stream.readAllBytes(), StandardCharsets.UTF_8);
 
-		String[] time = new String[Coin1_Value.length()];
-		String[] value = new String[Coin1_Value.length()];
+			// To print the string content
+			// System.out.println(contents);
 
-		for(int i=0; i<Coin1_Value.length(); i++)
-		{ 
-			JSONObject Current_stuff = Coin1_Value.getJSONObject(i);
-			time[i] = Current_stuff.get("created_at").toString();
-			value[i] = Current_stuff.get("price").toString();
+			JSONObject resJSON = new JSONObject(contents);
+			// System.out.println(resJSON);
+
+			JSONArray markets = resJSON.getJSONArray("markets");
+
+			for(int i = 0; i < markets.length(); i++)
+			{
+				// System.out.println(markets.get(i));
+				String curr_market = markets.get(i).toString();
+				JSONArray coin_value = new JSONArray(APIw.getJSONEndPoint("trades",curr_market));
+
+				JSONObject coin_allvals = new JSONObject();
+
+				String[] time = new String[coin_value.length()];
+				String[] value = new String[coin_value.length()];
+
+				for(int j=0; j<coin_value.length(); j++)
+				{ 
+					JSONObject Current_stuff = coin_value.getJSONObject(j);
+					time[j] = Current_stuff.get("created_at").toString();
+					value[j] = Current_stuff.get("price").toString();
+				}
+				
+				coin_allvals.put("time",time);
+				coin_allvals.put("value",value);
+				chart_stuff.put(curr_market,coin_allvals);
+			}
 		}
 		
-		Coin1_FinalValue.put("time",time);
-		Coin1_FinalValue.put("value",value);
-		chart_stuff.put("ethinr",Coin1_FinalValue);
+		catch(IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
+		
 		System.out.println(chart_stuff);
 	}
 }
